a43854138f9e130c19d3bc23ebfa16c60595397b
Rafał Selewońko <rafal@selewonko.com>
2014-08-29 15:47:45 +0200

    Changed behaviour so it does not replace template_name but rather looks first in directories appended by string in settings and then looks in normal folders so it now works with templates from external apps and gives ability to use common templates for different devices.

--- django_adaptive/filesystem.py
+++ django_adaptive/filesystem.py
@@ -1,10 +1,14 @@
 from django.template.loaders.filesystem import Loader as FilesystemLoader
+from django.conf import settings
 
-from .loader_utils import ParseTemplateName
+from .loader_utils import get_template_dirs
 
 
 class Loader(FilesystemLoader):
     def get_template_sources(self, template_name, template_dirs=None):
-        return super(Loader, self)\
-            .get_template_sources(ParseTemplateName(template_name),
-                                  template_dirs)
+        if template_dirs is None:
+            template_dirs = settings.TEMPLATE_DIRS
+
+        dirs = get_template_dirs(template_dirs)
+
+        return super(Loader, self).get_template_sources(template_name, dirs)
